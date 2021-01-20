2f41e4a5ef48954fe14250f0d3344a5d493e681d
Julian Saldana <julianesaldana@yahoo.com>
2021-01-20 10:02:17 -0800

    added first lab

---
+++ lab1.cpp
@@ -0,0 +1,15 @@
+// Julian Saldana
+// Class (CECS 282-06)
+// Project Name (Week 1 Lab 1)
+// Due Date (January 19, 2021)
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string greeting = "Hello World!";
+  cout << greeting << endl;
+  cout << "I love C++!\n";\
+  return 0;
+}
